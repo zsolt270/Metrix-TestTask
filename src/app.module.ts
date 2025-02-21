eda8c6677aee47ee0bfb8c69f988c82fcaeee9be
zsolt270 <zsolt270000@gmail.com>
2025-02-21 18:07:00 +0100

    Add config package and .env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [],
   providers: [],
 })
